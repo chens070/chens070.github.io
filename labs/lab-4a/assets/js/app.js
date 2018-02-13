23836be00668c5ddc9ccb599f24bb6173f43bdf0
Sheila Cheng <sheilacheng@Sheilas-MBP.wireless.newschool.edu>
2018-02-13 11:35:07 -0500

    Rare shop

---
+++ labs/lab-4a/assets/js/app.js
@@ -0,0 +1,19 @@
+function hello(name,age){
+  console.log('Name', name);
+  console.log('Age', age);
+}
+
+hello(987, 17);
+
+
+// // these are global variables
+// // var first_name ='Sheila',
+// //     last_name  ='Cheng',
+// //     full_name  = first_name + ' ' + last_name;
+
+// function hello(){
+//   // this is a local variable
+//   alert(full_name);
+// }
+
+// hello();
