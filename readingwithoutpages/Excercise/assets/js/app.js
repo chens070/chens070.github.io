f61b79f0d1926d4830a321b55b6b1c82195e6f3d
Sheila Cheng <sheilacheng@Sheilas-MBP.wireless.newschool.edu>
2018-03-13 10:39:06 -0400

    add lab 8

--- readingwithoutpages/Excercise/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("toggle-text");
-  if (x.innerHTML === "delicious!") {
-    x.innerHTML = "dangerous!";
-  } else {
-    x.innerHTML = "delicious!";
-  }
-}
